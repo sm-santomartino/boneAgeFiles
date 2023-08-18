10a1e69357b286e2c30efa765c195f3bf16d4fdc
Sam Santomartino <94193435+sm-santomartino@users.noreply.github.com>
2023-08-18 15:37:37 -0400

    Add files via upload

---
+++ transform/marker_images.py
@@ -0,0 +1,51 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from fontTools.ttLib import TTFont
+
+#file_dir = "DigitalHandAtlas/JPEGimages/"
+file_dir = "boneAgeFiles/RSNA_validation2/"
+
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles
+
+all_files = getListOfFiles(file_dir)
+
+for file in all_files:
+    try:
+        im = Image.open(file)
+
+        draw = ImageDraw.Draw(im)
+        
+        fnt = ImageFont.truetype("/System/Library/Fonts/Library/Fonts/Supplemental/Arial Bold.ttf", 235)
+        draw.text((50, 50), 'L', font=fnt, fill='white')
+
+        split = file.split('/')
+
+        #new_path = 'DHA_marker/'+split[2]+'/'+split[3]+'/'
+        new_path = 'rsna_marker/'
+
+        isExist = os.path.exists(new_path)
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs(new_path)
+            #print("The new directory is created!")
+
+        #im.save(new_path+split[4])
+        im.save(new_path+split[2])
+    except:
+        print('error: ' + file)
